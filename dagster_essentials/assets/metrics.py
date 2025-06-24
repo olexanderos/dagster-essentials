76194b7605af9a4d6aad0ee8a0266b71f1ff6922
Oleksandr Kosariev <alex.kosariev+1@gmail.com>
2025-06-24 23:07:35 +0200

    feat: ⚡create metrics using assets

--- dagster_essentials/assets/metrics.py
+++ dagster_essentials/assets/metrics.py
@@ -1,1 +1,33 @@
+import os
+
+import dagster as dg
+import duckdb
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+from dagster_essentials.assets import constants
+
+
+@dg.asset(deps=["taxi_trips", "taxi_zones"])
+def manhattan_stats() -> None:
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+            left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, "w") as output_file:
+        output_file.write(trips_by_zone.to_json())
 
