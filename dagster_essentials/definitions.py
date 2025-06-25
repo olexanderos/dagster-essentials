d6b4d9851723b736fc31991cdabc91afbcda434c
Oleksandr Kosariev <alex.kosariev+1@gmail.com>
2025-06-25 22:58:14 +0200

    feat: 🚩set up a database resource

--- dagster_essentials/definitions.py
+++ dagster_essentials/definitions.py
@@ -1,10 +1,14 @@
 import dagster as dg
 
 from dagster_essentials.assets import metrics, trips
+from dagster_essentials.resources import database_resource
 
 trip_assets = dg.load_assets_from_modules([trips])
 metric_assets = dg.load_assets_from_modules([metrics])
 
 defs = dg.Definitions(
     assets=[*trip_assets, *metric_assets],
+    resources={
+        "database": database_resource,
+    },
 )
